1a38598f2865ea3880d6570b9d20033039de6617
Angela Patino <angefraugela@gmail.com>
2025-01-17 16:50:03 +0100

    style: Uncomment code to display ui

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,8 @@
+import { fetchLatestInvoices } from "@/app/lib/data";
+import { LatestInvoice } from "@/app/lib/definitions";
+
 export default function Page() {
+
   return (
     <p>Invoices page</p>
   )
